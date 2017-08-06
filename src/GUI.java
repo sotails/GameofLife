1c3d4a645ae1d8cc976b6ab2f2053d6b7095727c
Owner <Owner@10.0.0.10>
2017-08-06 17:12:04 -0500

    Fixed issue with getting the GUI animated, but the GUI does not update correctly.

--- src/GUI.java
+++ src/GUI.java
@@ -1,13 +1,15 @@
 import java.awt.Color;
-
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.beans.Transient;
 
 import javax.swing.JPanel;
 
 public class GUI extends JPanel {
 	private Cell[][] board;
+
 	public GUI() {
-		
+
 		board = new Cell[100][100];
 	}
 
@@ -15,43 +17,58 @@ public class GUI extends JPanel {
 		return board;
 	}
 
-	
 	public void setBoard(Cell[][] x) {
 		board = x;
 	}
-	
+
+
+
+    
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Color gc = g.getColor();
-		 
+
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (board[i][j].getLife() == true) {
 					g.setColor(Color.blue);
-					g.fillRect(j, i, 1, 1);
+					g.fillRect(j*4, i*4, 4, 4);
 				}
 			}
 		}
 
 		g.setColor(gc);
 	}
+	
+
 
 	public int countSurrounding(Cell[][] xi, int a, int b) {
+		int z = 0;
+		int y = 0;
 		int count = 0;
-		int[][] surrounding = { { a - 1, b - 1 }, { a - 1, b }, { a - 1, b + 1 }, { a, b - 1 }, { a, b + 1 },
-				{ a + 1, b - 1 }, { a + 1, b }, { a + 1, b + 1 } };
-		for (int i[] : surrounding) {
+		int[][] i = { 
+				{ a - 1, b - 1 }, 
+				{ a - 1, b }, 
+				{ a - 1, b + 1 }, 
+				{ a, b - 1 }, 
+				{ a, b + 1 },
+				{ a + 1, b - 1 },
+				{ a + 1, b }, 
+				{ a + 1, b + 1 } };
+		for (int x =0; x<9;x++) {
+		
 			try {
-				if (xi[i[0]][i[1]].getLife() == true) {
+				if (xi[i[z][y]][i[z][y+1]].getLife() == true) {
 					count++;
 				}
 			} catch (ArrayIndexOutOfBoundsException e) {
 			}
+			z++;
 		}
 		return count;
 	}
 
-	public Cell[][] newUpdate(Cell[][] x) {
+	public void newUpdate(Cell[][] x) {
 		Cell[][] n = new Cell[100][100];
 		for (int i = 0; i < x.length; i++) {
 			for (int j = 0; j < x[i].length; j++) {
@@ -61,7 +78,7 @@ public class GUI extends JPanel {
 		for (int i = 0; i < x.length; i++) {
 			for (int j = 0; j < x[0].length; j++) {
 
-				if (countSurrounding(x, i, j) < 2 || countSurrounding(x, i, j) > 3) {
+				if ((countSurrounding(x, i, j) < 2) || (countSurrounding(x, i, j) > 3)) {
 					n[i][j].setLife(false);
 				} else {
 					n[i][j].setLife(true);
@@ -69,7 +86,13 @@ public class GUI extends JPanel {
 
 			}
 		}
-		return x;
+		for (int i = 0; i < x.length; i++) {
+			for (int j = 0; j < x[0].length; j++) {
+				this.getBoard()[i][j] = n[i][j];
+			}
+		}
+		
+
 	}
 
 }
--- src/Life.java
+++ src/Life.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,25 +18,30 @@ public class Life {
 		for (int i = 0; i < bore.getBoard().length; i++) {
 			for (int j = 0; j < bore.getBoard()[i].length; j++) {
 				double x = Math.random() * 1;
-				if (x >= .8) {
+				if (x >= .95) {
 					bore.getBoard()[i][j].setLife(true);
 					;
 				}
 			}
 		}
-		
+
 		JFrame mainFrame = new JFrame();
 		mainFrame.getContentPane().add(bore);
+		mainFrame.setSize(400,400);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setVisible(true);
-		new Timer(100, new ActionListener() {
-
-			
-			public void actionPerformed(ActionEvent e) {
-				bore.newUpdate(bore.getBoard());
-				bore.repaint();
-			}
-		}).start();
+		mainFrame.setLocationByPlatform(true);
+		mainFrame.setVisible(true);
+		ActionListener al = new ActionListener() {
+				
+				public void actionPerformed(ActionEvent e) {
+					bore.newUpdate(bore.getBoard());
+					bore.repaint();
+				}
+			};
+		
+		Timer tim = new Timer(5000,al);
+		tim.start();
+		while(true);
 	}
 
 }
